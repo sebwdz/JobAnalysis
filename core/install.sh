96218852cb3d672f988dd722cf9126aa167642d7
sebastien <sebastien.pouilly@epitech.eu>
2017-03-12 02:49:06 +0100

    clean

--- core/install.sh
+++ core/install.sh
@@ -1,5 +1,5 @@
 export WORKON_HOME=$HOME/code/.virtualenvs
-export PROJECT_HOME=$HOME/Project/JobTrendsCore/core
+export PROJECT_HOME=$HOME/code
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.5
 export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
